246bee2d4f96949b0801c435dd1cdebea0069908
vkm-dev <58807645+vkm-dev@users.noreply.github.com>
2021-07-08 18:07:57 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -68,3 +68,108 @@ function readURL(input) {
 $("body").on("change", "#pic_btn", function() {
     readURL(this);
 });
+
+// javascript search in table and recalculate sum in table foote
+$(document).ready(function(){
+    var noSearchClass = ['no-search','child']; // search will not be made in tr having this class (like table header)
+    $(".jsSearchInTable").on("keyup", function(){
+        // variable to calculte and manage sum in table footer
+        var trSumFooter = '';
+        var totalSum = [];
+        var filteredTrList = [];
+        var hiddenColsIndex = [];
+        var numericColsIndex = [];
+        var currencyColsIndex = [];
+        var txtAlignClsColIndex = [];
+        var currency = "{{session('currency')}}";
+        if ($("#reportData table thead tr.mainHeader").length > 0) {
+            var allCols = $("#reportData table thead tr.mainHeader th");
+        } else {
+            var allCols = $("#reportData table thead tr th");
+        }
+        $.each(allCols, function(i,th){
+            if($(th).hasClass('numericCol')) {
+                numericColsIndex.push(i);
+            }
+            if($(th).hasClass('d-none') || $(th).css('display') == 'none') {
+                hiddenColsIndex.push(i);
+            }
+            if($(th).hasClass('text-center')) {
+                txtAlignClsColIndex[i] = 'text-center';
+            } else if($(th).hasClass('text-left')) {
+                txtAlignClsColIndex[i] = 'text-left';
+            } else if($(th).hasClass('text-right')) {
+                txtAlignClsColIndex[i] = 'text-right';
+            } else {
+                txtAlignClsColIndex[i] = '';
+            }
+        });
+
+        //console.log(allCols,numericColsIndex,hiddenColsIndex);
+        var q = $(this).val();
+        var searchT;
+        var trList = $("#reportData table tr");
+        if (q.length > 0) {
+            clearTimeout(searchT);
+            searchT = setTimeout(function(){
+                $.each(trList, function(i,tr){
+                    if (tr.parentNode.nodeName != 'THEAD') {
+                        var innerTxt = tr.innerText;
+                        innerTxt = innerTxt.toString().replace("\t", " ");
+                        if (innerTxt.toLowerCase().indexOf(q.toLowerCase()) < 0) {
+                            tr.style.display = 'none';
+                        } else {
+                            filteredTrList.push(tr); // get all filtered tr 
+                            tr.style.display='';
+                        }
+                        // if last row of table and row is for showing total sum of all rows data
+                        if(i == trList.length - 1 && innerTxt.toLowerCase().indexOf('total') < 0) {
+                            tr.style.display = 'none';
+                        }
+                    }
+                });
+                // Code to calculate sum in table footer
+                if (numericColsIndex) {
+                    // initialize sum array
+                    for (i=0;i<allCols.length;i++) {
+                        totalSum[i] = "";
+                    }
+                    // calculate sum and store in array
+                    $.each(filteredTrList, function(i,tr){
+                        var tds = tr.children;
+                        //console.log(tds,numericColsIndex);
+                        $.each(numericColsIndex, function(i,v){
+                            let totStr = tds[v].innerText;
+                            if (totStr.toString().indexOf(currency) >= 0) {
+                               currencyColsIndex[v] = v;
+                            }
+                            totStr = totStr.replace(currency,"");
+                            totStr = totStr.toString().trim();
+                            let tot = parseFloat(totStr);
+                            let oldSum = (totalSum[v] == "") ? 0 : totalSum[v];
+                            totalSum[v] = (oldSum + tot);
+                        });
+                    });
+                    // create tr element to be appended in last 
+                    for (i=0;i<allCols.length;i++) {
+                        var hidden = hiddenColsIndex.includes(i) ? "style='display:none;'" : '';
+                        let moneySign = currencyColsIndex.includes(i) ? currency : '';
+                        let totalAmt = currencyColsIndex.includes(i) ?  totalSum[i].toFixed(2): totalSum[i];
+                        let txtAlignCls = txtAlignClsColIndex[i];
+                        trSumFooter += `
+                           <td ${hidden} class="${txtAlignCls} text-bold">${moneySign + totalAmt}</td>     
+                        `;
+                    }
+                    trSumFooter = `<tr class="jsSumTr">${trSumFooter}</tr>`;
+                    $("#reportData table").append(trSumFooter);
+                    //console.log(totalSum);
+                }
+            }, 10);
+        } else {
+            trList.css("display","");
+            $("#reportData table tr.jsSumTr").remove();
+        }
+
+    });
+
+});
